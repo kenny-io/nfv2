61498943e6f28ecb7502dc850a0eea0a8d37f465
kenny-io <ekeneeze3@gmail.com>
2023-10-17 15:21:17 +0400

    init a

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -13,16 +13,7 @@
 import { Config, Context } from '@netlify/functions';
 
 export default async (req: Request, context: Context) => {
-  const { city, country } = context.geo;
-  console.log(city, country);
-
-  try {
-    // return city and country to the client
-    return new Response(`Travel guide for ${city} in ${country}!`);
-  } catch (error) {
-    console.log(error);
-    return new Response('Something went wrong', { status: 500 });
-  }
+  return new Response(`Your IP is ${context.ip}`);
 };
 
 export const config: Config = {
